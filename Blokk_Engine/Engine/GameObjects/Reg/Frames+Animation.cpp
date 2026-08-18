f47864cdd60b1c7d695d1cb08f294418db18ec6a
hammykat <hammykat.2013@gmail.com>
2026-08-18 12:13:29 -0400

    Updated to match new architecture

--- Blokk_Engine/Engine/GameObjects/Reg/Frames+Animation.cpp
+++ Blokk_Engine/Engine/GameObjects/Reg/Frames+Animation.cpp
@@ -1,29 +1,26 @@
 #include "GameObject.hpp"
 
-void SetFrameNum(uint32_t FrameNum)
+void GameObject::SetFrameNum(uint32_t FrameNum)
 {
-    UpdateEngineData(
+    UpdateEngineData<uint32_t>(
         CommandTypes::Set,
-        (IsVisible)? &EngineObjects->VisibleFrameNums : 
-            &EngineObjects->InvisibleFrameNums,
-        EngineVisIdx,
+        &EngineObjects->FrameNums,
+        EngineIdx,
         FrameNum
-    )
+    );
 }
 
-void SetAnimation(string AnimName)
+void GameObject::SetAnimation(string AnimName)
 {
     auto It = EngineObjects->AnimNames.find(AnimName);
 
     if (It == EngineObjects->AnimNames.end())
         return;
 
-    UpdateEngineData(
+    UpdateEngineData<uint32_t>(
         CommandTypes::Set,
-        IsVisible
-            ? &EngineObjects->VisibleAnimNums
-            : &EngineObjects->InvisibleAnimNums,
-        EngineVisIdx,
+        &EngineObjects->AnimNums,
+        EngineIdx,
         It->second
     );
 }
