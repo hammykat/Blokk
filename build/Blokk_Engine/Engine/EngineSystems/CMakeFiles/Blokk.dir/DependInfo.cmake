e34d5d771be6fdf5e4d68be8b33654997c1a7e67
hammykat <hammykat.2013@gmail.com>
2026-08-18 11:57:07 -0400

    Compiled successfully with CMake

---
+++ build/Blokk_Engine/Engine/EngineSystems/CMakeFiles/Blokk.dir/DependInfo.cmake
@@ -0,0 +1,31 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/hammy/Blokk___Game-Engine/Blokk_Engine/Engine/EngineSystems/Animations.cpp" "Blokk_Engine/Engine/EngineSystems/CMakeFiles/Blokk.dir/Animations.cpp.obj" "gcc" "Blokk_Engine/Engine/EngineSystems/CMakeFiles/Blokk.dir/Animations.cpp.obj.d"
+  "C:/Users/hammy/Blokk___Game-Engine/Blokk_Engine/Engine/EngineSystems/IncrementFrames.cpp" "Blokk_Engine/Engine/EngineSystems/CMakeFiles/Blokk.dir/IncrementFrames.cpp.obj" "gcc" "Blokk_Engine/Engine/EngineSystems/CMakeFiles/Blokk.dir/IncrementFrames.cpp.obj.d"
+  "C:/Users/hammy/Blokk___Game-Engine/Blokk_Engine/Engine/EngineSystems/UserUpdates.cpp" "Blokk_Engine/Engine/EngineSystems/CMakeFiles/Blokk.dir/UserUpdates.cpp.obj" "gcc" "Blokk_Engine/Engine/EngineSystems/CMakeFiles/Blokk.dir/UserUpdates.cpp.obj.d"
+  "C:/Users/hammy/Blokk___Game-Engine/Blokk_Engine/Engine/EngineSystems/Velocities.cpp" "Blokk_Engine/Engine/EngineSystems/CMakeFiles/Blokk.dir/Velocities.cpp.obj" "gcc" "Blokk_Engine/Engine/EngineSystems/CMakeFiles/Blokk.dir/Velocities.cpp.obj.d"
+  "C:/Users/hammy/Blokk___Game-Engine/Blokk_Engine/Engine/EngineSystems/Visibility.cpp" "Blokk_Engine/Engine/EngineSystems/CMakeFiles/Blokk.dir/Visibility.cpp.obj" "gcc" "Blokk_Engine/Engine/EngineSystems/CMakeFiles/Blokk.dir/Visibility.cpp.obj.d"
+  "C:/Users/hammy/Blokk___Game-Engine/Blokk_Engine/Engine/GameObjects/Reg/Frames+Animation.cpp" "Blokk_Engine/Engine/EngineSystems/CMakeFiles/Blokk.dir/__/GameObjects/Reg/Frames+Animation.cpp.obj" "gcc" "Blokk_Engine/Engine/EngineSystems/CMakeFiles/Blokk.dir/__/GameObjects/Reg/Frames+Animation.cpp.obj.d"
+  "C:/Users/hammy/Blokk___Game-Engine/Blokk_Engine/Engine/GameObjects/Reg/Position.cpp" "Blokk_Engine/Engine/EngineSystems/CMakeFiles/Blokk.dir/__/GameObjects/Reg/Position.cpp.obj" "gcc" "Blokk_Engine/Engine/EngineSystems/CMakeFiles/Blokk.dir/__/GameObjects/Reg/Position.cpp.obj.d"
+  "C:/Users/hammy/Blokk___Game-Engine/Blokk_Engine/Engine/GameObjects/Reg/Velocity.cpp" "Blokk_Engine/Engine/EngineSystems/CMakeFiles/Blokk.dir/__/GameObjects/Reg/Velocity.cpp.obj" "gcc" "Blokk_Engine/Engine/EngineSystems/CMakeFiles/Blokk.dir/__/GameObjects/Reg/Velocity.cpp.obj.d"
+  "C:/Users/hammy/Blokk___Game-Engine/Blokk_Engine/Engine/GameObjects/Reg/Visibility.cpp" "Blokk_Engine/Engine/EngineSystems/CMakeFiles/Blokk.dir/__/GameObjects/Reg/Visibility.cpp.obj" "gcc" "Blokk_Engine/Engine/EngineSystems/CMakeFiles/Blokk.dir/__/GameObjects/Reg/Visibility.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/Blokk_Engine/Engine/EngineSystems/CMakeFiles/Blokk.dir/cmake_clean.cmake
@@ -0,0 +1,27 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Blokk.dir/Animations.cpp.obj"
+  "CMakeFiles/Blokk.dir/Animations.cpp.obj.d"
+  "CMakeFiles/Blokk.dir/IncrementFrames.cpp.obj"
+  "CMakeFiles/Blokk.dir/IncrementFrames.cpp.obj.d"
+  "CMakeFiles/Blokk.dir/UserUpdates.cpp.obj"
+  "CMakeFiles/Blokk.dir/UserUpdates.cpp.obj.d"
+  "CMakeFiles/Blokk.dir/Velocities.cpp.obj"
+  "CMakeFiles/Blokk.dir/Velocities.cpp.obj.d"
+  "CMakeFiles/Blokk.dir/Visibility.cpp.obj"
+  "CMakeFiles/Blokk.dir/Visibility.cpp.obj.d"
+  "CMakeFiles/Blokk.dir/__/GameObjects/Reg/Frames+Animation.cpp.obj"
+  "CMakeFiles/Blokk.dir/__/GameObjects/Reg/Frames+Animation.cpp.obj.d"
+  "CMakeFiles/Blokk.dir/__/GameObjects/Reg/Position.cpp.obj"
+  "CMakeFiles/Blokk.dir/__/GameObjects/Reg/Position.cpp.obj.d"
+  "CMakeFiles/Blokk.dir/__/GameObjects/Reg/Velocity.cpp.obj"
+  "CMakeFiles/Blokk.dir/__/GameObjects/Reg/Velocity.cpp.obj.d"
+  "CMakeFiles/Blokk.dir/__/GameObjects/Reg/Visibility.cpp.obj"
+  "CMakeFiles/Blokk.dir/__/GameObjects/Reg/Visibility.cpp.obj.d"
+  "libBlokk.a"
+  "libBlokk.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Blokk.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
