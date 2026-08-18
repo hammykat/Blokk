dd3321fc17aaba228fcf686396b5f2c08575675a
hammykat <hammykat.2013@gmail.com>
2026-08-18 00:33:12 -0400

    * Removed separate static and dynamic position vector handling * Updated SetPosition to use the unified XPositions and YPositions vectors * Changed SetPosition to use UpdateEngineData_Double for updating both coordinates * Updated SetXPosition to use the unified XPositions vector * Updated SetYPosition to use the unified YPositions vector * Updated GetPosition to retrieve both coordinates from the unified position vectors * Removed the old ChangePosition(Vector2) implementation * Reimplemented ChangePosition(float, float) using UpdateEngineData_Double * Re-added ChangePosition(Vector2) as a wrapper around ChangePosition(float, float) * Updated ChangeXPosition to use the unified XPositions vector * Updated ChangeYPosition to use the unified YPositions vector * Removed conditional logic for selecting static versus dynamic position storage * Updated position operations to use the unified EngineIdx instead of EngineVelIdx * Significantly reduced duplicated position update code between static and dynamic objects

--- Blokk_Engine/Engine/GameObjects/Reg/Position.cpp
+++ Blokk_Engine/Engine/GameObjects/Reg/Position.cpp
@@ -3,26 +3,13 @@
 // Set
 void GameObject::SetPosition(float TargetX, float TargetY) 
 {
-    // Set X pos
-    if(IsStatic) 
-    {
-        // Set x
-        UpdateEngineData(CommandTypes::Set, 
-            &EngineObjects->StaticXPositions, EngineVelIdx, TargetX);
-
-        // Set y
-        UpdateEngineData(CommandTypes::Set, 
-            &EngineObjects->StaticYPositions, EngineVelIdx, TargetY);
-    } else 
-    {
-        // Set x
-        UpdateEngineData(CommandTypes::Set, 
-            &EngineObjects->DynamicXPositions, EngineVelIdx, TargetX);
-
-        // Set y
-        UpdateEngineData(CommandTypes::Set, 
-            &EngineObjects->DynamicYPositions, EngineVelIdx, TargetY);
-    }
+    // Update X and Y
+    UpdateEngineData_Double(CommandTypes::Set, 
+            &EngineObjects->XPositions, 
+            &EngineObjects->YPositions,
+            TargetX, TargetY,
+            EngineIdx
+        );
 }
 void GameObject::SetPosition(Vector2 TargetPos) {
     SetPosition(TargetPos.x, TargetPos.y);
@@ -30,136 +17,46 @@ void GameObject::SetPosition(Vector2 TargetPos) {
 
 void GameObject::SetXPosition(float Target) 
 {
-    // If static
-    if(IsStatic) 
-    {
-        UpdateEngineData(CommandTypes::Set, 
-            &EngineObjects->StaticXPositions, EngineVelIdx, Target);
-    } 
-    else // If dynamic 
-    {
-        UpdateEngineData(CommandTypes::Set, 
-            &EngineObjects->DynamicXPositions, EngineVelIdx, Target);
-    }
+    UpdateEngineData(CommandTypes::Set, 
+            &EngineObjects->XPositions, EngineIdx, Target);
 }
 void GameObject::SetYPosition(float Target) 
 {
     // Set
-    if(IsStatic) {
-        UpdateEngineData(CommandTypes::Set, 
-            &EngineObjects->StaticYPositions, EngineVelIdx, Target);
-    } else {
-        UpdateEngineData(CommandTypes::Set, 
-            &EngineObjects->DynamicYPositions, EngineVelIdx, Target);
-    }
+    UpdateEngineData(CommandTypes::Set, 
+            &EngineObjects->YPositions, EngineIdx, Target);
 }
 
 // Get
 Vector2 GameObject::GetPosition() 
 {
-    // If the object is static
-    if(IsStatic)
-    {
-        // Get from the static object position data
-        return Vector2(
-            EngineObjects->StaticXPositions[EngineVelIdx],
-            EngineObjects->StaticYPositions[EngineVelIdx]
-        );
-    }
-    else // If the object is dynamic
-    {
-        // Get from the dynamic object data
-        return Vector2(
-            EngineObjects->DynamicXPositions[EngineVelIdx],
-            EngineObjects->DynamicYPositions[EngineVelIdx]
-        );
-    }
+    return Vector2(
+        EngineObjects->XPositions[EngineIdx],
+        EngineObjects->YPositions[EngineIdx]
+    );
 }
 
 // Change
-void GameObject::ChangePosition(Vector2 Change) 
-{
-    // If the object is static 
-    if(IsStatic) {
-        // Add to the static position data
-
-        // X
-        UpdateEngineData(CommandTypes::Add, 
-            &EngineObjects->StaticXPositions, EngineVelIdx, Change.x);
-
-        // Y
-        UpdateEngineData(CommandTypes::Add, 
-            &EngineObjects->StaticYPositions, EngineVelIdx, Change.y);
-    } 
-    else // If the object is dynamic  
-    {
-        // Add to the dynamic position data
-
-        // X
-        UpdateEngineData(CommandTypes::Add, 
-            &EngineObjects->DynamicXPositions, EngineVelIdx, Change.x);
-
-        // Y
-        UpdateEngineData(CommandTypes::Add, 
-            &EngineObjects->DynamicYPositions, EngineVelIdx, Change.y);
-    }
-}
 void GameObject::ChangePosition(float ChangeX, float ChangeY) 
 {
-    // If the object is static 
-    if(IsStatic) 
-    {
-        // Add to the static position data
-
-        // X
-        UpdateEngineData(CommandTypes::Add, 
-            &EngineObjects->StaticXPositions, EngineVelIdx, ChangeX);
-
-        // Y
-        UpdateEngineData(CommandTypes::Add, 
-            &EngineObjects->StaticYPositions, EngineVelIdx, ChangeY);
-    } 
-    else // If the object is dynamic  
-    {
-        // Add to the dynamic position data
-
-        // X
-        UpdateEngineData(CommandTypes::Add, 
-            &EngineObjects->DynamicXPositions, EngineVelIdx, ChangeX);
-
-        // Y
-        UpdateEngineData(CommandTypes::Add, 
-            &EngineObjects->DynamicYPositions, EngineVelIdx, ChangeY);
-    }
+    UpdateEngineData_Double(CommandTypes::Add, 
+        &EngineObjects->XPositions,
+        &EngineObjects->YPositions, 
+        ChangeX, ChangeY,
+        EngineIdx
+    );
+}
+void GameObject::ChangePosition(Vector2 Change) {
+    ChangePosition(Change.x, Change.y);
 }
 
 void GameObject::ChangeXPosition(float X) 
 {
-    // If the object is static 
-    if(IsStatic) {
-        UpdateEngineData(CommandTypes::Add, 
-            &EngineObjects->StaticXPositions, EngineVelIdx, X);
-    } 
-    else // If the object is dynamic  
-    {
-        // Add to the dynamic position data
-        UpdateEngineData(CommandTypes::Add, 
-            &EngineObjects->DynamicXPositions, EngineVelIdx, X);
-    }
+    UpdateEngineData(CommandTypes::Add, 
+        &EngineObjects->XPositions, EngineIdx, X);
 }
 void GameObject::ChangeYPosition(float Y) 
 {
-    // If the object is static 
-    if(IsStatic) 
-    {
-        // Add to the static position data
-        UpdateEngineData(CommandTypes::Add, 
-            &EngineObjects->StaticYPositions, EngineVelIdx, Y);
-    } 
-    else // If the object is dynamic 
-    {
-        // Add to the dynamic position data
-        UpdateEngineData(CommandTypes::Add, 
-            &EngineObjects->DynamicYPositions, EngineVelIdx, Y);
-    }
+    UpdateEngineData(CommandTypes::Add, 
+        &EngineObjects->YPositions, EngineIdx, Y);
 }
