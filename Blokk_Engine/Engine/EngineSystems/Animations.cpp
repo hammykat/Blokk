37117ce3095a5282663daea7e5299db436281ea7
hammykat <hammykat.2013@gmail.com>
2026-08-18 00:09:47 -0400

    Created basic animation structure

--- Blokk_Engine/Engine/EngineSystems/Animations.cpp
+++ Blokk_Engine/Engine/EngineSystems/Animations.cpp
@@ -1,6 +1,57 @@
 #include "EngineClassData.hpp"
 
-void CreateNewEmptyAnimation(string Name)
+void ObjectManager::CreateNewEmptyAnimation(string Name)
 {
-    
+    AnimNames[Name] = Frames.size();
+    Frames.emplace_back();
+    FrameWidths.emplace_back();
+    FrameHeights.emplace_back();
+}
+
+void ObjectManager::AddFramesToAnimation(string Name, vector<Texture2D>& TFrames)
+{
+    // Helpers
+    uint32_t AnimIdx = AnimNames[Name];
+    vector<Texture2D>& TargetAnim = Frames[AnimIdx];
+
+    // Insert frames
+    TargetAnim.insert(
+        TargetAnim.end(),
+        TFrames.begin(),
+        TFrames.end()
+    );
+
+    // Helpers
+    vector<uint32_t>& Widths = FrameWidths[AnimIdx];
+    vector<uint32_t>& Heights = FrameHeights[AnimIdx];
+
+    // Loop through frames
+    for(auto& Frame : TFrames)
+    {
+        // Add width + height
+        Widths.push_back(Frame.width);
+        Heights.push_back(Frame.height);
+    }
+}
+
+void ObjectManager::CreateAnimation(string Name, vector<Texture2D>& TFrames)
+{
+    uint32_t Idx = Frames.size();
+    AnimNames[Name] = Idx;
+    Frames.push_back(TFrames);
+
+    FrameWidths.emplace_back();
+    FrameHeights.emplace_back();
+
+    // Helpers
+    vector<uint32_t>& Widths = FrameWidths[Idx];
+    vector<uint32_t>& Heights = FrameHeights[Idx];
+
+    // Loop through frames
+    for(auto& Frame : TFrames)
+    {
+        // Add width + height
+        Widths.push_back(Frame.width);
+        Heights.push_back(Frame.height);
+    }
 }
