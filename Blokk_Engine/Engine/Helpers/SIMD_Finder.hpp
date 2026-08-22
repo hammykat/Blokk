57e995dbc75260fda99d369b34d1c756ad53eb86
hammykat <hammykat.2013@gmail.com>
2026-08-22 16:54:00 -0400

    Removed support for AVX (128 bit) and NEON, updated to correctly find

--- Blokk_Engine/Engine/Helpers/SIMD_Finder.hpp
+++ Blokk_Engine/Engine/Helpers/SIMD_Finder.hpp
@@ -1,96 +1,84 @@
 #pragma once
-#include <intrin.h>
+
+#include <cstdint>
+
+#if defined(__GNUC__) || defined(__clang__)
+    #include <cpuid.h>
+#endif
 
 enum class SIMDLevel
 {
     SSE2,
-    AVX,
+
     AVX2,
-    AVX512
+    AVX512,
+
+    Unsupported
 };
 
-inline SIMDLevel DetectSIMD()
-{
-    int cpuInfo[4];
+#if defined(_MSC_VER)
 
-    // Find the highest CPUID leaf supported by the CPU.
-    __cpuid(cpuInfo, 0);
-    int highestLeaf = cpuInfo[0];
+    #include <intrin.h>
 
-    // SSE2
-    // On x86-64, SSE2 is guaranteed, but check it anyway.
-    bool hasSSE2 = false;
-
-    if (highestLeaf >= 1)
+    inline SIMDLevel DetectSIMD()
     {
+        int cpuInfo[4];
+
+        // Get highest supported CPUID leaf.
+        __cpuid(cpuInfo, 0);
+        int highestLeaf = cpuInfo[0];
+
+        // We need CPUID leaf 1 to check SSE2.
+        if (highestLeaf < 1)
+            return SIMDLevel::Unsupported;
+
         __cpuid(cpuInfo, 1);
 
         // EDX bit 26 = SSE2
-        hasSSE2 = (cpuInfo[3] & (1 << 26)) != 0;
+        bool hasSSE2 =
+            (cpuInfo[3] & (1 << 26)) != 0;
+
+        if (!hasSSE2)
+            return SIMDLevel::Unsupported;
+
+        // AVX is intentionally disabled for now.
+        return SIMDLevel::SSE2;
     }
 
-    if (!hasSSE2)
-        return SIMDLevel::SSE2; // Your fallback would be scalar if needed.
+#elif defined(__GNUC__) || defined(__clang__)
 
-    if (highestLeaf < 1)
-        return SIMDLevel::SSE2;
+    inline SIMDLevel DetectSIMD()
+    {
+        #if defined(__x86_64__) || defined(__i386__)
 
-    // CPUID leaf 1
-    __cpuid(cpuInfo, 1);
+            // x86/x86-64.
+            // AVX is intentionally disabled for now.
 
-    // ECX bit 28 = AVX
-    bool hasAVX = (cpuInfo[2] & (1 << 28)) != 0;
+            if (__builtin_cpu_supports("sse2"))
+                return SIMDLevel::SSE2;
 
-    // ECX bit 27 = OSXSAVE
-    bool hasOSXSAVE = (cpuInfo[2] & (1 << 27)) != 0;
+            return SIMDLevel::Unsupported;
 
-    if (!hasAVX || !hasOSXSAVE)
-        return SIMDLevel::SSE2;
+        #elif defined(__aarch64__) || defined(_M_ARM64)
 
-    // Check which extended registers the OS actually saves/restores.
-    unsigned __int64 xcr0 = _xgetbv(0);
+            // ARM64 is not supported yet.
+            // NEON support can be added later.
+            return SIMDLevel::Unsupported;
 
-    // XMM state (bit 1) + YMM state (bit 2)
-    bool osSupportsAVX = (xcr0 & 0x6) == 0x6;
+        #else
 
-    if (!osSupportsAVX)
-        return SIMDLevel::SSE2;
+            // Unknown architecture.
+            return SIMDLevel::Unsupported;
 
-    // At this point AVX is usable.
-    if (highestLeaf < 7)
-        return SIMDLevel::AVX;
+        #endif
+    }
 
-    // CPUID leaf 7, subleaf 0
-    __cpuidex(cpuInfo, 7, 0);
+#else
 
-    // EBX bit 5 = AVX2
-    bool hasAVX2 = (cpuInfo[1] & (1 << 5)) != 0;
+    // Unknown compiler/platform.
+    inline SIMDLevel DetectSIMD()
+    {
+        return SIMDLevel::Unsupported;
+    }
 
-    if (!hasAVX2)
-        return SIMDLevel::AVX;
-
-    // AVX2 is usable.
-    // Now check AVX-512.
-    //
-    // EBX bit 16 = AVX-512 Foundation
-    bool hasAVX512F = (cpuInfo[1] & (1 << 16)) != 0;
-
-    if (!hasAVX512F)
-        return SIMDLevel::AVX2;
-
-    // AVX-512 requires the OS to enable:
-    //
-    // bit 1 = XMM
-    // bit 2 = YMM
-    // bit 5 = opmask
-    // bit 6 = ZMM_Hi256
-    // bit 7 = Hi16_ZMM
-    //
-    // 0xE6 = 1110 0110
-    bool osSupportsAVX512 = (xcr0 & 0xE6) == 0xE6;
-
-    if (!osSupportsAVX512)
-        return SIMDLevel::AVX2;
-
-    return SIMDLevel::AVX512;
-}
+#endif
