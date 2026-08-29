02d13bb01710743711d5fe3aa3ba59178669bbec
hammykat <hammykat.2013@gmail.com>
2026-08-28 20:19:13 -0400

    fix: implement object creation and destruction
    - Add object destruction with static and dynamic object handling
    - Add object instance pointers during creation
    - Split static and dynamic object swapping
    - Update object engine indexes after swaps
    - Process destroy commands through DestroyObject
    - Rename ProcessAddCommands to ProcessAddCommand

--- Blokk_Engine/Engine/EngineSystems/UserUpdates.cpp
+++ Blokk_Engine/Engine/EngineSystems/UserUpdates.cpp
@@ -4,13 +4,52 @@
 #include "GameTypes.hpp"
 #include "GameObject.hpp"
 
+void ObjectManager::DestroyObject(uint32_t ObjIdx) 
+{
+    // If object is static
+    if(ObjIdx >= DynamicObjectCount)
+    {
+        // Swap with the last item
+        SwapStaticObjects(ObjIdx, ObjectCount - 1);
+
+        // Remove the object
+        XPositions.pop_back();
+        YPositions.pop_back();
+        ObjectInstances.pop_back();
+
+        StaticObjectCount--;
+    }
+    else // If the object is dynamic 
+    {
+        // Swap with the last item
+        SwapDynamicObjects(ObjIdx, DynamicObjectCount - 1);
+
+        if(StaticObjectCount > 0)
+        {
+            // Swap with the last static item
+            SwapStaticObjects(DynamicObjectCount - 1, ObjectCount - 1);
+        }
+
+        // Remove the object from the back
+        XPositions.pop_back();
+        YPositions.pop_back();
+        XVelocities.pop_back();
+        YVelocities.pop_back();
+        ObjectInstances.pop_back();
+
+        DynamicObjectCount--;
+    }
+
+    ObjectCount--;
+}
+
 template <ConfiguredUpdateType T>
 void ObjectManager::ProcessFieldUpdateCommand(FieldUpdate<T> Command)
 {
     switch(Command.Type) 
     {
         case CommandTypes::Destroy:
-            
+            DestroyObject(Command.Idx);
             break;
 
         case CommandTypes::Set:
@@ -68,7 +107,7 @@ void ObjectManager::ProcessDoubleUpdateCommand(DoubleFieldUpdate<float> Command)
     }
 }
 
-void ObjectManager::ProcessAddCommands(ObjectCreationParams Fields)
+void ObjectManager::ProcessAddCommand(ObjectCreationParams Fields)
 {
     float XVel = Fields.Velocity.x;
     float YVel = Fields.Velocity.y;
@@ -81,6 +120,9 @@ void ObjectManager::ProcessAddCommands(ObjectCreationParams Fields)
         // Add position
         XPositions.push_back(Fields.Position.x);
         YPositions.push_back(Fields.Position.y);
+        
+        // Add instance pointer
+        ObjectInstances.push_back(Fields.Object);
 
         StaticObjectCount++;
     } 
@@ -90,8 +132,14 @@ void ObjectManager::ProcessAddCommands(ObjectCreationParams Fields)
         XPositions.push_back(Fields.Position.x);
         YPositions.push_back(Fields.Position.y);
 
-        // Swap position to keep with dynamic object data
-        SwapObjects(DynamicObjectCount, ObjectCount - 1);
+        // Add instance pointer
+        ObjectInstances.push_back(Fields.Object);
+
+        if(StaticObjectCount > 0)
+        {
+            // Swap position with first static member to keep with dynamic object data
+            SwapStaticObjects(DynamicObjectCount, ObjectCount - 1);
+        }
 
         // Add velocity
         XVelocities.push_back(XVel);
@@ -103,7 +151,7 @@ void ObjectManager::ProcessAddCommands(ObjectCreationParams Fields)
     ObjectCount++;
 }
 
-void ObjectManager::SwapObjects(uint32_t Obj1, uint32_t Obj2)
+void ObjectManager::SwapStaticObjects(uint32_t Obj1, uint32_t Obj2)
 {
     // Swap X and Y
     std::swap(XPositions[Obj1], XPositions[Obj2]);
@@ -117,3 +165,20 @@ void ObjectManager::SwapObjects(uint32_t Obj1, uint32_t Obj2)
     ObjectInstances[Obj2]->EngineIdx = Obj2;
 }
 
+void ObjectManager::SwapDynamicObjects(uint32_t Obj1, uint32_t Obj2)
+{
+    // Swap X and Y
+    std::swap(XPositions[Obj1], XPositions[Obj2]);
+    std::swap(YPositions[Obj1], YPositions[Obj2]);
+
+    // Swap velocities
+    std::swap(XVelocities[Obj1], XVelocities[Obj2]);
+    std::swap(YVelocities[Obj1], YVelocities[Obj2]);
+
+    // Swap instance pointers
+    std::swap(ObjectInstances[Obj1], ObjectInstances[Obj2]);
+
+    // Update object's internal vars
+    ObjectInstances[Obj1]->EngineIdx = Obj1;
+    ObjectInstances[Obj2]->EngineIdx = Obj2;
+}
