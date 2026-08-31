3ada3efcc53161c29bdabb7a378cdc0a5a5b1c45
hammykat <hammykat.2013@gmail.com>
2026-08-31 17:53:27 -0400

    Added visibility implementation with direct engine data manipulation and basic functions

--- Blokk_Engine/Engine/GameObjects/Reg/Visibility.cpp
+++ Blokk_Engine/Engine/GameObjects/Reg/Visibility.cpp
@@ -1,93 +1,44 @@
-/* #include "GameObject.hpp"
+#include "GameObject.hpp"
 
 namespace Blokk {
 
 // Set to visible
-    void SetToVisible() // Assumes that the object was invisible before
-    {
-        // Update local var
-        IsVisible = true;
+void GameObject::Show() // Assumes that the object was invisible before
+{
+    // Update local var
+    IsVisible = true;
 
-        // Get into visible
-        UpdateEngineData<bool>(
-            CommandTypes::Set,
-            &EngineObjects->IsVisible
-        );
-    }
+    // Set to visible
+    Engine->IsVisible[EngineIdx] = true;
+}
+
+// Set to invisible
+void GameObject::Hide() // Assumes that the object was visible before
+{
+    // Update local var
+    IsVisible = false;
 
     // Set to invisible
-    void SetToInvisible() // Assumes that the object was visible before
-    {
-        // Update local var
-        IsVisible = false;
-
-        // Remove from visible
-        
-    }
-
-void ObjectManager::Hide() 
-{
-    // If visible
-    if(IsVisible)
-    {
-        // Set to invisible
-        SetToInvisible();
-    }
-    
-    // If not visible, do nothing
+    Engine->IsVisible[EngineIdx] = false;
 }
 
-void ObjectManager::Show() 
-{
-    // If is already visible
-    if(IsVisible)
-    {
-        // Do nothing
-        return;
-    }
-    else // If hidden
-    {
-        // Set to visible
-        SetToVisible();
-    }
-}
-
-void ObjectManager::SetVisible(bool Vis) 
-{
-    // If not visible and setting to visible
-    if(!IsVisible && Vis) 
-    {
-        SetToVisible();
-    }
-    // If visible and being set to invisible
-    elif(IsVisible && !Vis)
-    {
-        // Set to invisible
-        SetToInvisible();
-    }
-
-    // Update local var
-    IsVisible = Vis;
-}
-
-bool ObjectManager::IsVisible() {
+bool GameObject::IsCurrentlyVisible() {
     return IsVisible;
 }
 
-void ObjectManager::ToggleVisibility() 
+void GameObject::ToggleVisibility() 
 {
     // If visible
     if(IsVisible)
     {
         // Set to invisible
-        SetToInvisible();
+        Hide();
     }
     else // If not visible
     {
         // Set to visible
-        SetToVsisible();
+        Show();
     }
 }  
     
 }
-*/
