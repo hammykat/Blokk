cf24bef6f0c48d51c5af92e7220b7855579d11d4
hammykat <hammykat.2013@gmail.com>
2026-08-31 17:52:36 -0400

    Replaced engine queue update calls with direcct engine data mutation

--- Blokk_Engine/Engine/GameObjects/Reg/Frames+Animation.cpp
+++ Blokk_Engine/Engine/GameObjects/Reg/Frames+Animation.cpp
@@ -4,27 +4,19 @@ namespace Blokk {
 
 void GameObject::SetFrameNum(uint32_t FrameNum)
 {
-    UpdateEngineData<uint32_t>(
-        CommandTypes::Set,
-        &EngineObjects->FrameNums,
-        EngineIdx,
-        FrameNum
-    );
+    // Set val
+    Engine->FrameNums[EngineIdx] = FrameNum;
 }
 
 void GameObject::SetAnimation(string AnimName)
 {
-    auto It = EngineObjects->AnimNames.find(AnimName);
+    auto It = Engine->AnimNames.find(AnimName);
 
-    if (It == EngineObjects->AnimNames.end())
+    if (It == Engine->AnimNames.end())
         return;
 
-    UpdateEngineData<uint32_t>(
-        CommandTypes::Set,
-        &EngineObjects->AnimNums,
-        EngineIdx,
-        It->second
-    );
+    // Set val
+    Engine->AnimNums[EngineIdx] = It->second;
 }
 
 }
